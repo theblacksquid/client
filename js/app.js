dfef747b9a35043cf3838bbc8eaabf5ea7e3c060
reganlawton <reganlawton92@gmail.com>
2015-01-11 20:32:52 +0930

    New file structure for views, and started sub_menu

--- js/app.js
+++ js/app.js
@@ -17,10 +17,10 @@ app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         // Home
         .when("/", {templateUrl: "views/home.html", controller: "homeCtrl"})
-        .when("/buy", {templateUrl: "views/buy.html", controller: "buyCtrl"})
-        .when("/sell", {templateUrl: "views/sell.html", controller: "sellCtrl"})
-        .when("/tools", {templateUrl: "views/tools.html", controller: "toolsCtrl"})
-        .when("/settings", {templateUrl: "views/settings.html", controller: "settingsCtrl"})
+        .when("/buy", {templateUrl: "views/buy/index.html", controller: "buyCtrl"})
+        .when("/sell", {templateUrl: "views/sell/index.html", controller: "sellCtrl"})
+        .when("/tools", {templateUrl: "views/tools/index.html", controller: "toolsCtrl"})
+        .when("/settings", {templateUrl: "views/settings/index.html", controller: "settingsCtrl"})
         // Show 404
         .otherwise("/404", {templateUrl: "views/_404.html", controller: "errorCtrl"});
 }]);
